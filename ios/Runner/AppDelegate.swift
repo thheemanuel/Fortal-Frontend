19b516b10eba50a6942e440213814719ab38af7d
theopeo <138706642+thheemanuel@users.noreply.github.com>
2025-08-28 18:03:53 +0200

    Adding files...

---
+++ ios/Runner/AppDelegate.swift
@@ -0,0 +1,31 @@
+import UIKit
+import Flutter
+import flutter_local_notifications
+
+@UIApplicationMain
+@objc class AppDelegate: FlutterAppDelegate {
+  override func application(
+    _ application: UIApplication,
+    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+  ) -> Bool {
+    GeneratedPluginRegistrant.register(with: self)
+
+  // NOTIFIKATIONER
+  // This is required to make any communication available in the action isolate.
+    FlutterLocalNotificationsPlugin.setPluginRegistrantCallback { (registry) in
+        GeneratedPluginRegistrant.register(with: registry)
+    }
+  if #available(iOS 10.0, *) {
+    UNUserNotificationCenter.current().delegate = self as? UNUserNotificationCenterDelegate
+  }
+  
+  if(!UserDefaults.standard.bool(forKey: "Notification")) {
+    UIApplication.shared.cancelAllLocalNotifications()
+    UserDefaults.standard.set(true, forKey: "Notification")
+}
+    /* GeneratedPluginRegistrant.register(with: self) */
+
+    // NOTIFIKATIONER SLUT
+    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+  }
+}
